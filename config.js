9be0ef6a61d35d17bc2e6fcdc85fb2d84bd46114
Ezekiel <ezekiel@gmail.com>
2025-04-20 12:54:10 +0800

    Updated XYZ feature / bugfix / etc

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    GOOGLE_CLIENT_ID: '824956744352-a4sj5egukjh1csk8galsalp6v4i73gbq.apps.googleusercontent.com',
-    SESSION_SECRET: 'y@9jLx9!*A4k!@Sg7b2Q$2uH9v7C4e5H',
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    SESSION_SECRET: process.env.SESSION_SECRET,
     DB_HOST: process.env.DB_HOST,       // Using MYSQLHOST from Render's environment
     DB_USER: process.env.DB_USER,       // Using MYSQLUSER from Render's environment
     DB_PASSWORD: process.env.DB_PASSWORD, // Using MYSQLPASSWORD from Render's environment
--- login.js
+++ login.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://student-management-frontend-xhec.vercel.app',
     credentials: true
 }));
 

4b70a04abb7a269936076e4e83df2992241ee88e
Ezekiel <ezekiel@gmail.com>
2025-05-07 22:19:59 +0800

    Updated XYZ feature / bugfix / etc

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ app.use(cors({
         'https://student-management-frontend-blav8pkj3-verkiel-cmds-projects.vercel.app'
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
